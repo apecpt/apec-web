881827ea57833fb93aeeaa06e88bd160d5d4f08e
Rui Batista <ruiandrebatista@gmail.com>
2015-12-29 15:14:50 +0000

    Add search form to nav bar.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,25 +5,27 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import pt.org.apec.services.books.common._
-import models.BooksClient
+import models._
 import godiva.core.pagination._
+import play.api.i18n._
 
-class Application @Inject() (booksClient : BooksClient) extends Controller {
+class Application @Inject() (booksClient : BooksClient, val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index = Action.async {
     val pagination = PaginationRequest(Page(1,10), true)
     for {
       ((categories, authors), publications) <- booksClient.getCategories zip booksClient.getAuthors zip booksClient.getPublications(pagination)
     } yield {
-    Ok(views.html.index(categories, authors, publications.elements))
+    Ok(views.html.index(GlobalData(None, MainForms.searchForm),IndexData(categories,  publications.elements)))
     }
   }
 
   def search(page: Long, query: String) = Action.async { implicit request =>
+    val globalData = GlobalData(None, MainForms.searchForm.bind(Map("q" -> query)))
     val pagination = PaginationRequest(Page(page, 10), true)
     for {
       publications <- booksClient.searchPublications(pagination, query)
-    } yield(Ok(views.html.search(publications, query)))
+    } yield(Ok(views.html.search(publications, query, globalData)))
   }
   
 }
--- app/models/PageData.scala
+++ app/models/PageData.scala
@@ -1,5 +1,12 @@
 package models
 
+import play.api.data._
 import pt.org.apec.services.books.common._
+import pt.org.apec.services.users.common._
 
-case class PageData(categories: Seq[WithPublicationCount[Category]] = Seq.empty)
+case class UserData(username: String, userId: UserId,profile: UserProfile)
+
+case class GlobalData(user: Option[UserData], searchForm: Form[String])
+
+case class IndexData(categories: Seq[WithPublicationCount[Category]], publications: Seq[PublicationInfo])
+case class CatalogData(categories: Seq[WithPublicationCount[Category]] = Seq.empty, authors: Seq[Author] = Seq.empty, statuses: Seq[PublicationStatus] = Seq.empty)
